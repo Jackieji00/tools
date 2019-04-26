692fda2f222134e094f68818dc5de465d8ebcb20
Jackieji00 <39365273+Jackieji00@users.noreply.github.com>
2019-04-26 14:37:54 -0500

    Add files via upload

--- gradechecker/grade.java
+++ gradechecker/grade.java
@@ -13,7 +13,18 @@ class grade{
     storeArray[sl-1] = v;
   }
   public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
     System.out.println("Welcome to the grade system!");
-    System.out.println();
+    System.out.println("what do you want to do?");
+    System.out.println("----------------------------");
+    System.out.println("1: check existing catergory with its weight");
+    System.out.println("2: add catergory with its weight");
+    System.out.println("3: add grade to the catergory");
+    System.out.println("Now you need to type this type assignment or quizs name.");
+    System.out.println("Name: ")
+    String name = scanner.nextLine();
+    System.out.println("How much does it weight?")
+    double w = scanner.nextDouble()
+
   }
 }
